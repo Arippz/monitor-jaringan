83e7a0426200620eae989ad23f12e2b82ea122a0
Radit220 <radithardi02@gmail.com>
2026-06-26 21:45:24 +0700

    array list data wifi

--- backend/main.java
+++ backend/main.java
@@ -1,5 +1,16 @@
-class Main {
+import java.util.ArrayList;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArrayList<PenggunaWiFi> databaseWifi = new ArrayList<>();
+
+        databaseWifi.add(new PenggunaWiFi("A1:B2:C3:D4", "John Doe", "120 MB", "Aktif", "10:00"));
+        databaseWifi.add(new PenggunaWiFi("E5:F6:G7:H8", "Budi Jatmiko", "45 MB", "Limit", "14:00"));
+        databaseWifi.add(new PenggunaWiFi("BC:D1:12:F5", "Siti Aminah", "0 MB", "DIBLOKIR", "14:00"));
+
+        // Cara mengakses datanya:
+        for (PenggunaWiFi p : databaseWifi) {
+            System.out.println("Nama: " + p.nama + " | Status: " + p.status);
+        }
     }
 }

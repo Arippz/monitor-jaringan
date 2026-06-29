f2c04af0bf52b2b9d3a3f2513e746e38176a5406
Arippz <ahmadarifinmaulidan123@gmail.com>
2026-06-29 08:21:57 +0700

    looping for the int is done

--- app-project/src/backend/Main.java
+++ app-project/src/backend/Main.java
@@ -1,22 +1,23 @@
 package backend;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
-        System.out.print(DBWifi.databaseWifi.get(1));
-
+        Perubahan();
     }
     public static void Perubahan(){
         for (int i = 0; i < 5; i++) {
             System.out.println("\n--- Second " + (i + 1) + " ---");
 
-            for (ArrayList<String> row : databaseWifi) {
+            for (ArrayList<String> row : DBWifi.databaseWifi) {
                 int currentNumber = Integer.parseInt(row.get(2).replaceAll("[^0-9]", ""));
                 row.set(2, (currentNumber + 3) + " MB");
                 System.out.println(row.get(1) + ": " + row.get(2));
             }
-
+            
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {

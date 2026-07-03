1a0c252bcaac7473c27bb71532d64421a7dd1289
Arippz <ahmadarifinmaulidan123@gmail.com>
2026-07-04 00:35:11 +0700

    pull end

--- app-project/src/backend/DBWifi.java
+++ app-project/src/backend/DBWifi.java
@@ -8,9 +8,9 @@ public class DBWifi {
     // Menambahkan baris data (sebagai record)
     static {
         databaseWifi.add(new ArrayList<>(Arrays.asList("A1:B2:C3:D4", "John Doe", "10 MB", "Aktif", "10:00")));
-        databaseWifi.add(new ArrayList<>(Arrays.asList("E5:F6:G7:H8", "Budi Jatmiko", "10 MB", "Limit", "14:00")));
-        databaseWifi.add(new ArrayList<>(Arrays.asList("BC:D1:12:F5", "Siti Aminah", "10 MB", "DIBLOKIR", "14:00")));
-        databaseWifi.add(new ArrayList<>(Arrays.asList("D9:E0:F1:G2", "User Baru", "10 MB", "Aktif", "15:00")));
+        databaseWifi.add(new ArrayList<>(Arrays.asList("E5:F6:G7:H8", "Budi Jatmiko", "10 MB", "Limit", "10:00")));
+        databaseWifi.add(new ArrayList<>(Arrays.asList("BC:D1:12:F5", "Siti Aminah", "10 MB", "DIBLOKIR", "10:00")));
+        databaseWifi.add(new ArrayList<>(Arrays.asList("D9:E0:F1:G2", "User Baru", "10 MB", "Aktif", "10:00")));
     }
 
     // Metode untuk menambahkan pengguna baru
